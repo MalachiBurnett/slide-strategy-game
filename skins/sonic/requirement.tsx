d36eb2abe6f63839a20a40185ae16b05fbfacb4d
Mali B <maliburnett@icloud.com>
2026-03-29 17:45:38 +0100

    fixed skins

--- skins/sonic/requirement.tsx
+++ skins/sonic/requirement.tsx
@@ -1,3 +1,4 @@
 export const progress = (user: any) => user.gamesPlayed || 0;
-export const checkUnlock = (user: any) => (user.gamesPlayed || 0) >= 5;
+export const checkUnlock = (user: any) => 
+(user.gamesPlayed || 0) >= 5;
 export const progressMax = (user: any) => 5;
